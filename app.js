1f6888fdaa3ed52d2cca0d732e62b6fde1b9b885
yuminbc <spdlxm9666@nate.com>
2021-12-02 14:51:08 +0900

    211202

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const query = document.getElementById('search')
+const url2 = window.location.href
+const url = new URL(url2);
+const urlParams = url.searchParams;
+
+
+const selectpoint = urlParams.get('keyword')
+
+const query= urlParams.get('w')
+// const search = document.getElementById('search')
 const submitBtn = document.getElementById('submit')
-// const container = document.getElementById('container')
-// const BASE_URL = 'http://localhost:5050/api/words'
-const BASE_URL = 'https://crawling-word-dic.herokuapp.com/api/words'
-  
-  
+
+const seloption = selectpoint
+console.log(seloption)
+console.log(query)
+// console.log(search)
+//    const container = document.getElementById('container')
+//    const BASE_URL = 'http://localhost:5000/api/words';
+const BASE_URL = 'http://localhost:5050/api/words'
+
+
+
 // 검색어에 특수문자가 들어간 경우 검색이 안되도록 함
 function checkIfStringHasSpecialCharacter(str) {
     const re = /[`!@#$%^&*()_+\-=\[\]{};':"\\|.<>\/?~]/;
@@ -21,89 +35,84 @@ function checkIfStringHasLetters(str){
 }
 
 function checkIfStringHasNull(str){
-    return null(str);
+    return str="null";
 }
 
 // 버튼 활성화
 function enableSubmitBtn(state){
     submitBtn.disabled = state
-}      
+}   
+
+
+
 // 서버 데이터 가져오기
-function getData(baseUrl, query){
-    enableSubmitBtn(true)//비활성화
-    console.log("서버 접속중.....")
-    //fetch `$접속할 url
-    
-    // if(chekNull(query)){
-    //     enableSubmitBtn(false)
-    //     container.innerHTML = "검색어를 입력하세요"
-    //     return;  
+function getData(baseUrl){
+
+    // if(query == undefined ||query == "undefined"){
+    //     query = "가"
     // }
 
-    //사용자 입력 유효성 검사
-    // if(checkIfStringHasNull(query)){
-    //     enableSubmitBtn(false) 
-    //    container.innerHTML = "검색어를 입력하세요"
-    //    return;
-    // }
-
-
-
-    if(checkIfStringHasSpecialCharacter(query)){
-        enableSubmitBtn(false)//활성화
-       alert("특수문자는 검색할수없습니다")
-       serend.innerHTML=""
-       container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
-       return;
+   // 서버 index.js 파일에 반드시 cors 옵션에 localhost:5500 주소를 허용한다고 설정해야 함
+   console.log(seloption)
+   console.log(query)
+//    console.log(search)
+    if(checkIfStringHasNull(query)){
+        return;
     }
-   
+
+   if(checkIfStringHasSpecialCharacter(query)){
+    enableSubmitBtn(false)//활성화
+    alert("특수문자는 검색할수없습니다")
+    serend.innerHTML=""
+    container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
+    window.history.back();
+    return ;
+    }
+
     if(checkIfStringHasNumbers(query)){
-       enableSubmitBtn(false)
-       alert("숫자는 검색할수없습니다")
-       serend.innerHTML=""
-       container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
-       return;
+    enableSubmitBtn(false)
+    alert("숫자는 검색할수없습니다")
+    serend.innerHTML=""
+    container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
+    window.history.back();
+    return ;
     }
     if(checkIfStringHasLetters(query)){
         enableSubmitBtn(false)
-       alert("영어는 검색할수없습니다")
-       serend.innerHTML=""
-       container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
-       return;
+    alert("영어는 검색할수없습니다")
+    serend.innerHTML=""
+    container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
+    window.history.back();
+    return ;
+    }
+    if(query== ""){
+        alert("검색어를 입력해주세요")
+        window.history.back();
+        return ;
     }
 
 
-
-    
-    fetch(`${baseUrl}/${query}`, {
+   fetch(`${baseUrl}/${seloption}/${query}`, {
         headers: {
             "Content-Type": "application/json",
+            // "Access-Control-Allow-Origin": "*" // 이 코드 때문에 CORS 에러가 발생한것임. 이 코드 주석처리하면 프론트엔드에서 곧바로 외부 API 접근가능하다. (프록시나 서버가 필요없음)
         }
-    })
-    .then(res => res.json())
-    .then(data => {
+   })
+   .then( res => res.json())
+   .then( data => {
         enableSubmitBtn(false)
-        // submitBtn.disabled = false
+        submitBtn.disabled = false
         console.log(data)
         const {words} = data;
-        //데이터 유효성 검증
-        if(query== ""){
-            alert("검색어를 입력해주세요")
+
+        if(words.length === 0){
+            serend.innerHTML=""
+            container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다";
             return;
         }
 
-        else if (words.length == 0){
-            container.innerHTML ="&quot;"+ query+"&quot;와 일치하는 항목이 없습니다"
-            return;
-        }
-
-
-        const template = words.map(word =>{
-
-            // if(isNaN(word.r_seq)){                       
-            //     word.r_seq="" ;
-            // }
-            return(
+        const template = words.map(word => {
+            return (
                 `
                 <div class="item">
                         <div class="word">
@@ -115,30 +124,38 @@ function getData(baseUrl, query){
                 </div>
                 `
             )
-        
         })
-        
-        container.innerHTML = template.join("")// DOM에 template 삽입
-        serend.innerHTML="&quot;"+query+"&quot;의 검색결과는 "+words.length+"개 입니다."+"<hr>"
+        if(seloption =="word"){
+            var seloptions  = "단어"; 
+        }
+        else if(seloption =="des"){
+            var seloptions  = "설명"; 
+        }
+        else if(seloption =="worddes"){
+            var seloptions  = "단어+설명"; 
+        }
+        container.innerHTML = template.join("")
+        serend.innerHTML="&quot;"+query+"&quot;의 " + seloptions + " 검색결과는 "+words.length+"개 입니다."+"<hr>"
         search.innerHTML=query
+        console.log(query)
 
+        
+    
     })
 }
 
 submitBtn.addEventListener('click', function(){
-    console.log(query.value)
-    // console.log(submitBtn)
-    //버튼 비활성화
-    // this.disabled = true
-    // setTimeout(getData(BASE_URL, query.value),5000000)//5초 50000밀리
-    getData(BASE_URL, query.value)
+//    console.log(this)
+        console.log(query.value)
+        getData(BASE_URL, query.value,selectpoint.value)
+  
 })
-query.addEventListener("keypress",function(e){
-    console.log('key pressed')
-    if(e.keyCode === 13){
-        getData(BASE_URL,query.value)
-    }
-})
-window.addEventListener('DoMCcontentLoaded',function(){
+
+
+window.addEventListener('DOMContentLoaded', function() { 
     getData(BASE_URL)
-})
+});
+
+search.value=query
+
+document.ft.keyword.value=seloption;
